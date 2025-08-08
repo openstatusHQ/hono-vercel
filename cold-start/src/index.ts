636b3c7bf94de129ba7b154293544552a8b31625
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-08-08 13:28:01 +0200

    🚀 init project

---
+++ cold-start/src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const welcomeStrings = [
+  'Hello Hono!',
+  'Trying something for https://www.openstatus.dev'
+]
+
+app.get('/', (c) => {
+  return c.text(welcomeStrings.join('\n\n'))
+})
+
+export default app
